58419b693af160c6f5fd3a6026b5f89399f0ec1f
ebram <ebram.ayman73@gmail.com>
2025-03-30 16:04:38 +0200

    Add Doctor model and adding view and controller for doctor and booking appointment

---
+++ Booking Doctors/Controllers/DoctorController.cs
@@ -0,0 +1,16 @@
+﻿using Booking_Doctors.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking_Doctors.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private readonly ApplicationDbContext _context = new();
+        
+        public IActionResult BookAppointment()
+        {
+            var doctors = _context.Doctors;
+            return View(doctors.ToList());
+        }
+    }
+}
---
+++ Booking Doctors/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Booking_Doctors.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking_Doctors.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-JUIUM6J\\SQLEXPRESS;Initial Catalog=BookingDoctors;Integrated Security=True;TrustServerCertificate=True"); 
+
+        }
+        
+    }
+}
